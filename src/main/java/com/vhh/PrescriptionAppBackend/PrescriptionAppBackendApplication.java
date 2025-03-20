6a40c029e25147e7c696f18a7cefb6e29d528356
VietHungHoang <viethunghoang1508@gmail.com>
2025-03-20 08:37:41 +0700

    Init project

---
+++ src/main/java/com/vhh/PrescriptionAppBackend/PrescriptionAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vhh.PrescriptionAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrescriptionAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrescriptionAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vhh/PrescriptionAppBackend/PrescriptionAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vhh.PrescriptionAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
